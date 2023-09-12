0cbfbbdc21651ef36d033d74d823889feb67e9a9
拓海 橋本 <hashimoto-takumi@ksc365.onmicrosoft.com>
2023-09-12 18:00:16 +0900

    第二回

--- client/src/types/prefecture.d.ts
+++ client/src/types/prefecture.d.ts
@@ -1,4 +1,16 @@
 // TODO: API定義書を参考に型定義すること
-export type Prefecture = {}
+export type Prefecture = {
+  prefCode: number
+  prefName: string
+}
 // TODO: Prefecture型にインターセクション型を利用して、チェックボックスのbooleanも追加すること
-export type PrefectureDisplay = {}
+export type PrefectureDisplay = Prefecture & {
+  isCheck: boolean
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const hoge: PrefectureDisplay = {
+  prefCode: 1,
+  prefName: "北海道",
+  isCheck: true
+}
